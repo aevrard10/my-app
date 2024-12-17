89c036bd47f66c6238d2eca2cb752305fb8796c1
aevrard10 <aevrard@eutech.fr>
2024-12-17 21:08:52 +0100

    wip

--- src/navigation/screens/Home/components/CardComponent/index.tsx
+++ src/navigation/screens/Home/components/CardComponent/index.tsx
@@ -1,23 +1,12 @@
-import {
-  Text,
-  Card,
-  Button,
-  Avatar,
-  AvatarIconProps,
-  Surface,
-} from "react-native-paper";
-import { StyleSheet, View } from "react-native";
+import { Card, Button } from "react-native-paper";
+import { StyleSheet } from "react-native";
 import { FC, useCallback } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { Reptile } from "@shared/graphql/utils/types/types.generated";
 import useRemoveReptileMutation from "../../hooks/mutations/useRemoveReptile";
 import { useQueryClient } from "@tanstack/react-query";
 import useReptilesQuery from "../../hooks/queries/useReptilesQuery";
-import { formatDDMMYYYY } from "@shared/utils/formatedDate";
 
-const LeftContent = (props: AvatarIconProps) => (
-  <Avatar.Icon {...props} icon="snake" />
-);
 type CardComponentProps = {
   item?: Reptile;
 };
@@ -49,27 +38,12 @@ const CardComponent: FC<CardComponentProps> = (props) => {
   }, [item, mutate]);
   return (
     <Card style={styles.card}>
-      <Card.Title
-        title={item?.name}
-        subtitle={item?.age + " ans"}
-        left={LeftContent}
-      />
-      <Card.Content>
-        <Text
-          style={{
-            flexWrap: "wrap",
-          }}
-          variant="titleLarge"
-        >
-          {item?.species}
-        </Text>
-        <Text variant="bodyMedium">{formatDDMMYYYY(item?.last_fed)}</Text>
-      </Card.Content>
       <Card.Cover
         source={{
           uri: item?.image_url,
         }}
       />
+      <Card.Title title={item?.name} subtitle={item?.age + " ans"} />
       <Card.Actions>
         <Button
           onPress={() =>
@@ -90,19 +64,6 @@ const CardComponent: FC<CardComponentProps> = (props) => {
 const styles = StyleSheet.create({
   card: {
     margin: 20,
-  },
-  container: {
-    marginTop: 10,
-    justifyContent: "center",
-    alignItems: "center",
-    gap: 10,
-    flexDirection: "row",
-  },
-  fab: {
-    position: "absolute",
-    margin: 16,
-    right: 0,
-    bottom: 0,
   },
 });
 export default CardComponent;
